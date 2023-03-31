73a50cfae07f6740fb0c2be2019e50a8ec467a46
Vile Deg <gvadim134@gmail.com>
2023-03-31 16:05:36 +0200

    Interpreter class

--- interpret/interp_classes.py
+++ interpret/interp_classes.py
@@ -1,22 +1,245 @@
+import io
+import re
+import xml.etree.ElementTree as et
+
 class Argument:
-    def __init__(self, arg_type, value):
+    def __init__(self, arg_type: str, value: str):
+        if arg_type not in ["int", "bool", "string", "nil", "label", "type", "var"]:
+            raise TypeError("Invalid argument type")
+        
+
         self.arg_type = arg_type
         self.value = value
 
-class Instuction:
-    def __init__(self, order, opcode, arg1, arg2, arg3):
+class Instruction:
+    
+
+
+
+
+    def __init__(self, order, opcode):
         self.order = order
         self.opcode = opcode
-        self.arg1 = arg1
-        self.arg2 = arg2
-        self.arg3 = arg3
+        self.arg1, self.arg2, self.arg3 = None, None, None
     
     def add_argument(self, arg_type, value):
-        if self.arg1 == None:
-            self.arg1 = (arg_type, value)
-        elif self.arg2 == None:
-            self.arg2 = (arg_type, value)
-        elif self.arg3 == None:
-            self.arg3 = (arg_type, value)
-        else:
-            raise Exception("Too many arguments")
+        try:
+            if self.arg1 == None:
+                self.arg1 = (arg_type, value)
+            elif self.arg2 == None:
+                self.arg2 = (arg_type, value)
+            elif self.arg3 == None:
+                self.arg3 = (arg_type, value)
+            else:
+                raise Exception("Too many arguments")
+        except BaseException:
+            raise
+
+class Singleton(type):
+    ''' 
+    Singleton metaclass.
+    Inspired by: https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python 
+    '''
+    _instances = {}
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+
+class Interpreter(metaclass=Singleton):
+    def _validate_xml(self, xml_tree: et.ElementTree):
+        extxt = "invalid xml"
+        try:
+            prog = xml_tree.getroot()
+            print(f"{prog.tag=}")
+            if prog.tag != "program":
+                raise Exception(extxt)
+            ra = list(prog.attrib.keys())
+            print(f"{ra=}")
+            if not('language' in ra) and prog.attrib['language'] != "IPPcode23": # TODO: description, name
+                raise Exception(extxt)
+            
+            for inst in prog:
+                print(f"\t{inst.tag=}")
+                if inst.tag != "instruction":
+                    raise Exception(extxt)
+                ca = list(inst.attrib.keys())
+                print(f"\t{ca=}")
+                if not('order' in ca) or not('opcode' in ca):
+                    raise Exception(extxt)
+                
+                for arg in inst:
+                    print(f"\t\t{arg.tag=}")
+                    if not(re.match(r"arg[123]", arg.tag)):
+                        raise Exception(extxt)
+                    sa = list(arg.attrib.keys())
+                    print(f"\t\t{sa=}")
+                    if not('type' in sa):
+                        raise Exception(extxt)
+                    
+                    print(f"\t\t{arg.text=}")
+            return prog
+        except BaseException:
+            self._error_code = 31
+            raise
+
+    def _fill_instructions(self, prog: et.ElementTree):
+        for inst in prog:
+            order = int(inst.attrib['order'])
+            opcode = inst.attrib['opcode']
+
+        try:
+            inst_obj = Instruction(order, opcode)
+
+            for arg in inst:
+                arg_type = arg.attrib['type']
+                value = arg.text
+                inst_obj.add_argument(arg_type, value)
+        except BaseException:
+            self._error_code = 32
+            raise
+
+        self._instructions.append(inst_obj)
+
+        # sort intructions by order
+        self._instructions.sort(key=lambda x: x.order)
+    
+    def i_move(self, inst: Instruction):
+        pass
+
+    def i_createframe(self, _: Instruction):
+        pass
+
+    def i_pushframe(self, _: Instruction):
+        pass
+
+    def i_popframe(self, inst: Instruction):
+        pass
+
+    def i_defvar(self, inst: Instruction):
+        pass
+    
+    def i_add(self, inst: Instruction):
+        pass
+
+    def i_sub(self, inst: Instruction):
+        pass
+
+    def i_mul(self, inst: Instruction):
+        pass
+
+    def i_idiv(self, inst: Instruction):
+        pass
+
+    def i_lt(self, inst: Instruction):
+        pass
+
+    def i_gt(self, inst: Instruction):
+        pass
+
+    def i_eq(self, inst: Instruction):
+        pass
+
+    def i_and(self, inst: Instruction):
+        pass
+
+    def i_or(self, inst: Instruction):
+        pass
+
+    def i_not(self, inst: Instruction):
+        pass
+
+    def i_int2char(self, inst: Instruction):
+        pass
+
+    def i_stri2int(self, inst: Instruction):
+        pass
+
+    def i_read(self, inst: Instruction):
+        pass
+
+    def i_write(self, inst: Instruction):
+        pass
+
+    def i_concat(self, inst: Instruction):
+        pass
+
+    def i_strlen(self, inst: Instruction):
+        pass
+
+    def i_getchar(self, inst: Instruction):
+        pass
+
+    def i_setchar(self, inst: Instruction):
+        pass
+
+    def i_type(self, inst: Instruction):
+        pass
+
+    def i_label(self, inst: Instruction):
+        pass
+
+    def i_jump(self, inst: Instruction):
+        pass
+
+    def i_jumpifeq(self, inst: Instruction):
+        pass
+
+    def i_jumpifneq(self, inst: Instruction):
+        pass
+
+    def i_exit(self, inst: Instruction):
+        pass
+
+    def i_dprint(self, inst: Instruction):
+        pass
+
+    def i_break(self):
+        pass
+
+    # def _define_opcodes(self):
+    #     self._opcodes = [
+    #         "move", "createframe", "pushframe", "popframe", "defvar", "call", "return", "pushs", "pops",
+    #         "add", "sub", "mul", "idiv", "lt", "gt", "eq", "and", "or", "not", "int2char", "str2int",
+    #         "read", "write", "concat", "strlen", "getchar", "type", "label", "jump", "jumpifeq", "jumpifneq",
+    #         "exit", "dprint", "break"
+    #     ]
+
+    #     # Assign lambda functions to opcode names
+    #     self._idict = {
+    #         "move": lambda self, inst: pass
+    #     }
+    def __init__(self):
+        self._source_file  = None
+        self._instructions = []
+        self._error_code   = 0
+
+    def initialize(self, source_file: io.TextIOWrapper):
+        self._source_file  = source_file
+        
+        #self._opcodes      = []
+        #self._idict        = {}
+
+        try:
+            xml_tree = et.parse(self._source_file)
+        except et.ParseError:
+            self._error_code = 31
+            raise
+        
+        try:
+            prog = self._validate_xml(xml_tree)
+            self._fill_instructions(prog)
+        except BaseException:
+            raise
+
+    
+
+    @property
+    def error_code(self):
+        return self.error_code
+    
+    def run(self):
+        for inst in self._instructions:
+            getattr(self, f"i_{inst.opcode}")(inst)
--- interpret/interpret.py
+++ interpret/interpret.py
@@ -1,12 +1,9 @@
-import re
-import argparse
-import xml.etree.ElementTree as et
 import sys
-import os
+import argparse
 
 from interp_classes import *
 
-def open_file(file_name, mode):
+def open_file(file_name: str, mode: str):
     try:
         f = open(file_name, mode, encoding="UTF-8")
     except IOError as e:
@@ -27,41 +24,6 @@ def parse_args():
         exit(10)
     return args
 
-def check_xml():
-    extxt = "invalid xml"
-    try:
-        prog = tree.getroot()
-        print(f"{prog.tag=}")
-        if prog.tag != "program":
-            raise Exception(extxt)
-        ra = list(prog.attrib.keys())
-        print(f"{ra=}")
-        if not('language' in ra) and prog.attrib['language'] != "IPPcode23": # TODO: description, name
-            raise Exception(extxt)
-        
-        for inst in prog:
-            print(f"\t{inst.tag=}")
-            if inst.tag != "instruction":
-                raise Exception(extxt)
-            ca = list(inst.attrib.keys())
-            print(f"\t{ca=}")
-            if not('order' in ca) or not('opcode' in ca):
-                raise Exception(extxt)
-            
-            for arg in inst:
-                print(f"\t\t{arg.tag=}")
-                if not(re.match(r"arg[123]", arg.tag)):
-                    raise Exception(extxt)
-                sa = list(arg.attrib.keys())
-                print(f"\t\t{sa=}")
-                if not('type' in sa):
-                    raise Exception(extxt)
-                
-                print(f"\t\t{arg.text=}")
-    except BaseException as e:
-        print("Error: " + str(e), file=sys.stderr)
-        exit(32)
-
 if __name__ == '__main__':
     # Parse arguments
     args = parse_args()
@@ -74,46 +36,12 @@ if __name__ == '__main__':
         except IOError as e:
             print("Error: " + str(e), file=sys.stderr)
             exit(11)
-
-    content = source_file.read()
-    print(content)
-    source_file.seek(0)
-
-    # exit(0)
-
-    # xml load
+    
+    intepreter_obj = Interpreter()
     try:
-        tree = et.parse(source_file)
-    except et.ParseError as e:
+        intepreter_obj.initialize(source_file)
+    except BaseException as e:
         print("Error: " + str(e), file=sys.stderr)
-        exit(31)
-        
-    # xml check
-    check_xml()    
-
-
-    exit(0)
-
-    # xml to instruction
-    instructions = []
-    for inst in prog:
-        order = int(inst.attrib['order'])
-        opcode = inst.attrib['opcode']
-        arg1 = None
-        arg2 = None
-        arg3 = None
-        for arg in inst:
-            arg_type = arg.attrib['type']
-            value = arg.attrib['value']
-            if arg.tag == "arg1":
-                arg1 = (arg_type, value)
-            elif arg.tag == "arg2":
-                arg2 = (arg_type, value)
-            elif arg.tag == "arg3":
-                arg3 = (arg_type, value)
-        instructions.append(Instuction(order, opcode, arg1, arg2, arg3))
-
-    # interpret instructions
-    for i in instructions:
-        interpret(i)
-
+        exit(intepreter_obj.error_code())
+    
+    intepreter_obj.run()
